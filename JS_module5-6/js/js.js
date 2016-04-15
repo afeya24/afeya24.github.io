29389ed2f945c03d70d495c6ab5f58e1ef5dccc1
Chernyavskaya Alyona <afeya24@yandex.ru>
2016-04-15 15:21:14 +0300

    add changes

--- JS_module5-6/js/js.js
+++ JS_module5-6/js/js.js
@@ -1,35 +1,92 @@
-﻿function makeTimer() {
-	var myTimer = document.getElementById ('my_timer');
-	var time = myTimer.innerHTML;
-	var array = time.split (':');
-	var h = array[0];
-	var min = array[1];
-	var s = array[2];
-setInterval(function qetHours () {
-	h++;
-	if (h < 10) {
-		h = '0' + h;
-	 };
-}, 3600000000);
-setInterval(function getMinutes () {
-	min++;
-	if (min < 10) {
-		 min = '0' + min;
-	};
-}, 60000);
-setInterval(function getSeconds () {
-	s++;
-	 if (s < 10) {
-		 s = '0' + s;
-	 };
-}, 1000);
-document.getElementById("my_timer").innerHTML = h+':'+min+':'+s;
-};
-window.addEventListener('load', makeTimer);
+﻿function Stopwatch(elem) {
+  var time = 0;
+  var interval;
+  var offset;
 
+  function update() {
+    if (this.isOn) {
+      var timePassed = delta();
+      time += timePassed;
+    }
 
+    var formattedTime = timeFormatter(time);
+    elem.textContent = formattedTime;
+  }
 
+  function delta() {
+    var now = Date.now();
+    var timePassed = now - offset;
+    offset = now;
+    return timePassed;
+  }
 
+  function timeFormatter(timeInMilliseconds) {
+    var time = new Date(timeInMilliseconds);
+    var minutes = time.getMinutes().toString();
+    var seconds = time.getSeconds().toString();
+    var milliseconds = time.getMilliseconds().toString();
 
+    if (minutes.length < 2) {
+      minutes = '0' + minutes;
+    }
 
+    if (seconds.length < 2) {
+      seconds = '0' + seconds;
+    }
 
+    while (milliseconds.length < 3) {
+      milliseconds = '0' + milliseconds;
+    }
+
+    return minutes + ' : ' + seconds + ' . ' + milliseconds;
+  }
+
+  this.isOn = false;
+
+  this.start = function() {
+    if (!this.isOn) {
+      interval = setInterval(update.bind(this), 1);
+      offset = Date.now();
+      this.isOn = true;
+    }
+  };
+
+  this.stop = function() {
+    if (this.isOn) {
+      clearInterval(interval);
+      interval = null;
+      this.isOn = false;
+    }
+  };
+
+  this.reset = function() {
+    time = 0;
+    update();
+  };
+}
+
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('toggle');
+var resetBtn = document.getElementById('reset');
+
+var watch = new Stopwatch(timer);
+
+function start() {
+  watch.start();
+  toggleBtn.textContent = 'Pause';
+}
+
+function stop() {
+  watch.stop();
+  toggleBtn.textContent = 'Continue';
+}
+
+toggleBtn.addEventListener('click', function() {
+  (watch.isOn) ? stop() : start();
+});
+
+resetBtn.addEventListener('click', function() { 
+    watch.reset();
+	watch.stop();
+	toggleBtn.textContent = 'Start';
+  });
