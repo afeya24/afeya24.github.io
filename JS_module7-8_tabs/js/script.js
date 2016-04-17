b8af8158d28b2f40739bb3579a9ea190709c8072
Chernyavskaya Alyona <afeya24@yandex.ru>
2016-04-17 11:06:47 +0300

    add 7-8

---
+++ JS_module7-8_tabs/js/script.js
@@ -0,0 +1,13 @@
+﻿$(function() {
+    $('ul.tabs__menu li:first').addClass ('active');
+    $('ul.tabs_text li:first').css ('display', 'block');
+    $('ul.tabs__menu li').on('click', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+		var $content = $('ul.tabs-text li');
+	$content.eq($(this).index()).css ('display', 'block')
+		.siblings().css ('display', 'none');
+		console.log($content.eq($(this).index()));
+		console.log($(this).index())
+		// 
+    })
+})
