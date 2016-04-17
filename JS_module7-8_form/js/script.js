895ddcbcd8debed1823a2a70b43590f7da2cfd2e
Chernyavskaya Alyona <afeya24@yandex.ru>
2016-04-17 12:48:01 +0300

    add module 7-8 form

---
+++ JS_module7-8_form/js/script.js
@@ -0,0 +1,17 @@
+﻿$(function(){
+        $('.input').hover(
+			function(event){
+				$(this).removeClass('none').addClass('block');
+			},
+			function(event){
+				$(this).removeClass('block').addClass('none');
+        }); 
+		$('button').on('click', function() {
+				$('.input').toggleClass('none');
+				if ($(this).html()=='Show prompts') {
+					$(this).html('Hide prompts')
+				} else {
+					$(this).html('Show prompts');
+				}
+		});
+		})
