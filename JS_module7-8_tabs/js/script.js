e6bd7d8fc2c259f8bb670c693b8798ce3f55c885
Chernyavskaya Alyona <afeya24@yandex.ru>
2016-04-17 12:46:08 +0300

    add changes

--- JS_module7-8_tabs/js/script.js
+++ JS_module7-8_tabs/js/script.js
@@ -1,13 +1,15 @@
 ﻿$(function() {
-    $('ul.tabs__menu li:first').addClass ('active');
-    $('ul.tabs_text li:first').css ('display', 'block');
-    $('ul.tabs__menu li').on('click', function() {
-        $(this).addClass('active').siblings().removeClass('active');
-		var $content = $('ul.tabs-text li');
-	$content.eq($(this).index()).css ('display', 'block')
-		.siblings().css ('display', 'none');
-		console.log($content.eq($(this).index()));
-		console.log($(this).index())
-		// 
+ var $tabs = $('ul.tabs__menu li');
+ 
+ $tabs.on('click', function() {
+  var $clickedTab = $(this);
+  var $contentId = $clickedTab.attr('data-id');
+  console.log($contentId);
+  
+  $tabs.removeClass('active');
+  $clickedTab.addClass('active');
+
+  $('#' + $contentId).addClass('active');
+  $('#' + $contentId).siblings().removeClass('active')
     })
 })
